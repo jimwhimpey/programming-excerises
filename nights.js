0b7ac13b6437b703d94365d90029ce9f0f37732f
jimwhimpey <jimwhimpey@me.com>
2015-06-14 10:59:52 -0700

    Initial commit

---
+++ nights.js
@@ -0,0 +1,13 @@
+/* 
+	Interview Questions
+	Provide a set of positive integers (an array of integers). Each integer represent number of 
+	nights user has requested at a hotel. If you are a host, you need to design and implement 
+	an algorithm to find out the maximum number a nights you can accommodate. The constrain 
+	is that you have to reserve at least one day between each request.
+
+	Example:
+	1) Input: [1, 2, 3] ===> output: 4, because you will pick 1 and 3
+	2) input: [5, 1, 2, 6] ===> output: 11, because you will pick 5 and 6
+	3) input: [5, 1, 2, 6, 20, 2] ===> output: 27, because you will pick 5, 2, 20
+*/
+
---
+++ word-find.js
@@ -0,0 +1,4 @@
+/*
+	Given a dictionary, and a matrix of letters, find all the words in the matrix 
+	that are in the dictionary. (Going across, down or diagonally)  
+*/
